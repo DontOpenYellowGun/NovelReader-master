02b458f4149c5b3eac1dd807fa55b5c789e7ccd7
姚志鹏 <sven775288@gmail.com>
2021-07-15 18:39:27 +0800

    xxx

---
+++ app/src/main/java/com/example/newbiechen/ireader/ui/base/BaseMVPActivity.java
@@ -0,0 +1,30 @@
+package com.example.newbiechen.ireader.ui.base;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Created by newbiechen on 17-4-25.
+ */
+
+public abstract class BaseMVPActivity<T extends BaseContract.BasePresenter> extends BaseActivity{
+
+    protected T mPresenter;
+
+    protected abstract T bindPresenter();
+
+    @Override
+    protected void processLogic() {
+        attachView(bindPresenter());
+    }
+
+    private void attachView(T presenter){
+        mPresenter = presenter;
+        mPresenter.attachView(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPresenter.detachView();
+    }
+}
